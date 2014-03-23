02be33b80f6ac37731e52cdff47c91a893b36d57
aok1425 <aok1425@gmail.com>
2014-03-22 22:06:20 -0700

    3

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, request, url_for, render_template, redirect, flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_input_form():
     return 'This should work!!!'
+	
+@app.route('/test')
+def show_this_page():
+    return render_template('input_form.html')
