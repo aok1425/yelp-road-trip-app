a4836b76274090d50df4db52d733831bb77a4c8c
aok1425 <aok1425@gmail.com>
2014-03-22 22:34:06 -0700

    14 fixed indentation

--- hello.py
+++ hello.py
@@ -28,11 +28,11 @@ def add_entry():
 # MY MASSIVE ASS SCRIPT
 #####
 # btwn time_to_restos(), time_to_restos_json(), and resto_table, I am betting that resto_table.keys() will always match up w/[sth out of the JSON?]. keys() is sometimes random though... 
- # schema of resto_table is [address,rating,# reviews,yelp link,rating img,duration to resto,distance to resto,minutes out of way,distance out of way]
+# schema of resto_table is [address,rating,# reviews,yelp link,rating img,duration to resto,distance to resto,minutes out of way,distance out of way]
  
- # if time_block using Bing Maps points < cull_block, cull_search_points() won't filter out any too-long steps
+# if time_block using Bing Maps points < cull_block, cull_search_points() won't filter out any too-long steps
  
- # Google Distance Matrix API has limit of 100 elements/query
+# Google Distance Matrix API has limit of 100 elements/query
  
 #len(yelp_json_to_table(yelp_search(20,radius=40000,location='fernley,nv')))
 #do_everything('reno,nv','jackpot,nv',7,5,'3:00pm','3:30pm')
