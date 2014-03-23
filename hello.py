20b0f6076c85ef7c32389ccfe0bccbfb7b501461
aok1425 <aok1425@gmail.com>
2014-03-22 22:21:43 -0700

    9

--- hello.py
+++ hello.py
@@ -13,10 +13,10 @@ def add_entry():
 	end=request.form['end']
 	time_leaving=request.form['time_leaving']
 	eating_time=request.form['eating_time']
-	logging_input_data.append([start,end,time_leaving,eating_time])
+	#logging_input_data.append([start,end,time_leaving,eating_time])
 
-	do_everything(start,end,7,5,time_leaving,eating_time,10,40,20,20)
-	make_HTML_file(start,end,filtered_table)
+	#do_everything(start,end,7,5,time_leaving,eating_time,10,40,20,20)
+	#make_HTML_file(start,end,filtered_table)
 	
 	return 'You want to start at '+start+', end at '+end+', leave at '+time_leaving+', and eat around '+eating_time+'.'
 	#return redirect(url_for('static', filename='map.html'))
