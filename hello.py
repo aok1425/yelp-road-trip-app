ec6611508fa94c92b4d61eb7dd8cccb05b64fde6
aok1425 <aok1425@gmail.com>
2014-03-22 22:17:03 -0700

    6

--- hello.py
+++ hello.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def show_input_form():
     return render_template('input_form.html')
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	start=request.form['start']
+	end=request.form['end']
+	time_leaving=request.form['time_leaving']
+	eating_time=request.form['eating_time']
+	logging_input_data.append([start,end,time_leaving,eating_time])
+
+	do_everything(start,end,7,5,time_leaving,eating_time,10,40,20,20)
+	make_HTML_file(start,end,filtered_table)
+	
+	return 'You want to start at '+a+', end at '+b+', leave at '+c+', and eat around '+d+'.'
+	#return redirect(url_for('static', filename='map.html'))
