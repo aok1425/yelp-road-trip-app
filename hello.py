61c92696e3f0d6c24b57e8e83ef85b4a89d33bd0
aok1425 <aok1425@gmail.com>
2014-03-25 01:12:50 -0700

    added speed markers

--- hello.py
+++ hello.py
@@ -45,6 +45,7 @@ def add_entry():
 	else:
 		print 'Regular option chosen.'
 		destination_time=check_time(start,end,time_leaving,eating_time)
+		print 'Checking the times...'
 		if destination_time=='yes':
 			logging_input_data.append([start,end,time_leaving,eating_time])
 			do_everything(start,end,20,20,time_leaving,eating_time,9,40,20,20) # GMaps Dist Matrix API can only handle 9
@@ -197,7 +198,7 @@ def filter_search_points_by_eating_time(search_points,eating_time_start,duration
 	eating_time_end=eating_time_start+duration*60*60 # 1 hour duration
 	eating_time_start=eating_time_start-time_back*60*60 # does yelp search at points up to a 1/4 hr before my eating time
 	filtered_search_points=[]
-
+	
 	for i in range(len(search_points)):
 		cumm_duration=search_points[i][0]
 
@@ -346,20 +347,27 @@ def do_everything(start,end,search_limit,return_limit,start_time,eating_time_sta
 	Review_cutoff is how many restos do you want to return after sorting them my # of reviews.
 
 	Around what time do you want to start eating? Program will look 1.5 hours after that in terms of places to look, not time to final destinations."""
+	print 'Pulling results from Google Directions'
 	result=get_gmaps_json(start,end)
 
 	start_time_repr=datetime.datetime.strptime(start_time, '%H:%M')
 	drive_duration=result['routes'][0]['legs'][0]['duration']['value'] # in seconds
 	#print 'You will arrive at',end,'at',datetime.datetime.strftime(start_time_repr+datetime.timedelta(seconds=drive_duration),'%I:%M%p')
 
+	print 'Making search points'
 	search_points=make_search_points(result,time_block,too_long_step)
+	print 'culling them'
 	search_points=cull_search_points(search_points,cull_block)
 	if just_best==False:
 		search_points=filter_search_points_by_eating_time(search_points,time_diff(start_time,eating_time_start))
 	else:
 		search_points=search_points[2:len(search_points)-3] # takes away two first and three last points. finds best restos along the way
 
-	for row in range(len(search_points)):
+	len_search_points=len(search_points)
+	counter=0
+	for row in range(len_search_points):
+		counter+=1
+		print 'Searching on Yelp point',counter,'out of',len_search_points
 		yelp_table_to_dict(yelp_json_to_table(yelp_search(search_limit,radius,latlong=turn_latlong_list_to_string(search_points[row][2]),sort_method=2)),return_limit)
 
 	filter_resto_table(resto_table, review_cutoff)
@@ -374,7 +382,7 @@ def do_everything(start,end,search_limit,return_limit,start_time,eating_time_sta
 		for j in result:
 			filtered_table[filtered_table.keys()[i]].append(j[i])
 			
-	print 'Done!'
+	print 'done'
 	#return search_points_to_return # can chg this to return previous search_points; input that into 'plot bing points on map.py'
 
 def convert_to_yelp_app_link(website_link):
@@ -382,125 +390,6 @@ def convert_to_yelp_app_link(website_link):
 	unique_id=website_link[17:]
 	yelp_link_start='yelp://'
 	return yelp_link_start+unique_id
-
-def make_HTML_file(start_point,end_point,time_leaving,resto_table,just_best=False):
-	"""Resto_addresses is a table of just addresses."""
-	# I'm assumong here that w/the dixt, the order will always be the same, so I can make mltuple lists out of it.
-	file=open('c:/users/alex/desktop/map.html','w')
-	#file=open(app.root_path+"/static/map.html",'w')
-	key='AIzaSyBsbGsLbD2hM5jr1bewKc6hotr3iV1lpmw'
-	locations=[] # many locations to put on map
-	infowindows=[]
-
-	### Part 0: Take dict resto_table and put JS infowindow code into a list on the HTML file
-	def fix_quotes(input):
-		"""Take a string that might contain single quotes and adds backslash before each one."""
-		# doesn't account for double quotes
-		output=[]
-		for i in range(len(input)):
-			if input[i]=="'":
-				output.append('\\')
-				output.append(input[i])
-			else:
-				output.append(input[i])
-		return ''.join(output)
-
-	def add_infowindow(resto,number,time_leaving,just_best=False):
-		resto_data=resto_table[resto]
-		
-		if just_best==False:
-			start_time_repr=datetime.datetime.strptime(time_leaving, '%H:%M')
-			resto_destination_time=datetime.datetime.strftime(start_time_repr+datetime.timedelta(seconds=resto_data[7]),'%I:%M%p')
-			resto_destination_time = resto_destination_time.lstrip('0')
-			resto_destination_time = resto_destination_time[:len(resto_destination_time)-2]+resto_destination_time[len(resto_destination_time)-2:].lower() # makes the last two characters lowercase
-			#print 'You will arrive at',end,'at',datetime.datetime.strftime(start_time_repr+datetime.timedelta(seconds=drive_duration),'%I:%M%p')
-		else:
-			resto_destination_time=int(resto_data[7]/60)
-
-		infowindow=range(22)
-		infowindow[0]="var contentString"
-		infowindow[1]=str(number)
-		infowindow[2]="= \n\'<h2 id=\"firstHeading\" class=\"firstHeading\">"
-		infowindow[3]=fix_quotes(resto)
-		infowindow[4]="</h2>\'+\n\'<img src=\""
-		infowindow[5]=str(resto_data[4])
-		infowindow[6]="\" alt=\"Yelp rating image\">\'+\n\'<p>"
-		infowindow[7]=str(resto_data[2])
-		infowindow[8]=" reviews</p>\'+\n\'<p>"
-		infowindow[9]=str("%0.1f" % (resto_data[8]*0.000621371)) # keepin this as 1 decimal place bc more important this be accurate
-		if just_best==False:
-			infowindow[10]=" mi away</p>'+\n\'<p>You will arrive at "
-			infowindow[11]=str(resto_destination_time)
-			infowindow[12]="</p>\'+\n\'<p>"
-		else:
-			infowindow[10]=" mi away</p>'+\n\'<p>You will arrive in "
-			infowindow[11]=str(resto_destination_time)
-			infowindow[12]=" mins.</p>\'+\n\'<p>"		
-		infowindow[13]=str(int(resto_data[6]*0.000621371)) 
-		infowindow[14]=" mi/"
-		infowindow[15]=str(int(float(resto_data[5])/60)) # converting to minutes
-		infowindow[16]=" min detour</p>\'+\n\'<a href=\""
-		infowindow[17]=str(resto_data[3])
-		infowindow[18]="\" target=\"\_blank\">visit Yelp page</a>\'+"
-		infowindow[19]="\n\'<p><a href=\""
-		infowindow[20]=convert_to_yelp_app_link(infowindow[17]) # which is the Yelp mobile link
-		infowindow[21]="\" target=\"\_blank\">view in iPhone app</a></p>\'"
-		return ''.join(infowindow)
-
-	number=0
-	for resto in resto_table:
-		number+=1
-		if just_best==False:
-			infowindows.append(add_infowindow(resto,number,time_leaving))
-		else:
-			infowindows.append(add_infowindow(resto,number,'placeholder',just_best=True))
-
-	### Part 1: Take resto addresses and add their coordinates to a JS list called locations on the HTML file.
-
-	def geocode_address(address,sensor='false'):
-		"""Input address and return lat long dictionary."""
-		payload = {'address':address,'sensor':sensor,'key': key}
-		site='https://maps.googleapis.com/maps/api/geocode/json'
-		r = requests.get(site, params=payload)
-		return r.json()
-
-	def show_coordinates(thejson):
-		"""Inputs Geoccoding JSON and returns the lat long dict."""
-		return thejson['results'][0]['geometry']['location']
-
-	def add_location(latlngdict,number):
-		"""Takes latlong dict and puts it as a string into locations. When written, this will be a JS location object."""
-		location=range(4)
-		location[0]='[contentString' # to change
-		location[1]=str(number)+', '
-		location[2]=str(latlngdict['lat'])+','+str(latlngdict['lng'])+','
-		location[3]=str(number)+'],'
-
-		return ''.join(location)
-
-	number=0
-	for resto in resto_table:
-		number+=1
-		resto_data=resto_table[resto]
-		locations.append(add_location(show_coordinates(geocode_address(resto_data[0])),number)) # 0 being the address
-
-
-	### Part 2: Take all the elements of the HTML file and write them.
-
-	list_of_elements=range(9)
-
-	list_of_elements[0]="<!DOCTYPE html>\n<html>\n  <head>\n    <meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\">\n    <meta charset=\"utf-8\">\n    <title>Directions service</title>\n    <style>\n      html, body, #map-canvas {\n        height: 100%;\n        margin: 0px;\n        padding: 0px\n      }\n      #panel {\n        position: absolute;\n        top: 5px;\n        left: 50%;\n        margin-left: -180px;\n        z-index: 5;\n        background-color: #fff;\n        padding: 5px;\n        border: 1px solid #999;\n      }\n    </style>\n    <script src=\"https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false\"></script>\n    <script>\n\tvar directionsDisplay;\n\tvar directionsService = new google.maps.DirectionsService();\n\tvar map;\n\n\tvar start_point = \'"
-	list_of_elements[1]=start_point #'2312,31321'
-	list_of_elements[2]="';\n\tvar end_point = '"
-	list_of_elements[3]=end_point #'2312,31321'
-	list_of_elements[4]="\';\n\n\tfunction initialize() {\n\t\tvar mapOptions = {\n\t\t  center: new google.maps.LatLng(13.412849, 103.868251),\n\t\t  zoom: 15\n\t\t};\n\t\t\n\t"
-	list_of_elements[5]='\n\n'.join(infowindows)
-	list_of_elements[6]="\n\n\tvar map = new google.maps.Map(document.getElementById(\"map-canvas\"),mapOptions);\n\n    var locations = [\n\t"
-	list_of_elements[7]='\n\t'.join(locations)
-	list_of_elements[8]="\n    ];\n\n    var infowindow = new google.maps.InfoWindow();\n\n    var marker, i;\n\n    for (i = 0; i < locations.length; i++) {  \n      marker = new google.maps.Marker({\n        position: new google.maps.LatLng(locations[i][1], locations[i][2]),\n        map: map\n      });\n\n      google.maps.event.addListener(marker, \'click\', (function(marker, i) {\n        return function() {\n          infowindow.setContent(locations[i][0]);\n          infowindow.open(map, marker);\n        }\n      })(marker, i));\n\t  \n\t  google.maps.event.addListener(map, \'click\', function() {\n\t\tinfowindow.close(map,marker);\n\t});\n    }\n\n\tdirectionsDisplay = new google.maps.DirectionsRenderer();\n\t  directionsDisplay.setMap(map);\n\t}\n\n\tfunction calcRoute() {\n\t\tvar request = {\n\t\t  origin:start_point,\n\t\t  destination:end_point,\n\t\t  travelMode: google.maps.TravelMode.DRIVING\n\t\t};\n\t\t\n\t\tdirectionsService.route(request, function(response, status) {\n\t\t\tif (status == google.maps.DirectionsStatus.OK) {\n\t\t\t  directionsDisplay.setDirections(response);\n\t\t\t}\n\t\t});\n\t}\n\n\tcalcRoute()\n\tgoogle.maps.event.addDomListener(window, \'load\', initialize);\n    </script>\n  </head>\n  <body>\n    <div id=\"map-canvas\"/>\n  </body>\n</html>"
-
-	for each in list_of_elements:
-		file.close()
 	
 def make_HTML_file(start_point,end_point,time_leaving,resto_table,just_best=False):
 	"""Resto_addresses is a table of just addresses."""
@@ -598,8 +487,10 @@ def make_HTML_file(start_point,end_point,time_leaving,resto_table,just_best=Fals
 		return ''.join(location)
 
 	number=0
+	len_resto_table=len(resto_table)
 	for resto in resto_table:
 		number+=1
+		print 'Geocoding address',number,'out of',len_resto_table
 		resto_data=resto_table[resto]
 		locations.append(add_location(show_coordinates(geocode_address(resto_data[0])),number)) # 0 being the address
 
