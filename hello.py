c95dbfb4312519b6bff24109e1c78a75fdd418c3
aok1425 <aok1425@gmail.com>
2014-06-30 18:17:42 -0700

    testing error_page on db

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ from python.write_map_file import *
 from python.write_results_file import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 @app.route('/')
@@ -64,7 +64,11 @@ def pageNotFound(error):
 	eating_time=request.form['eating_time']
 
 	print 'writing error file...'
-	db_entry = Search(get_my_ip(), datetime.datetime.now(), start, end, time_leaving, eating_time, False)
+	if request.form['button']=='Find great restaurants':
+		db_entry = Search(get_my_ip(), datetime.datetime.now(), start, end, 'I\'m feeling lucky', 'I\'m feeling lucky', False)
+	else:
+		db_entry = Search(get_my_ip(), datetime.datetime.now(), start, end, time_leaving, eating_time, False)
+		
 	db.session.add(db_entry)
 	db.session.commit()		
 	
