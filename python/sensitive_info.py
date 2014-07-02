7e78eef813e9cf4cfc0cf1a28885354f5f5de2a9
Alexander Ko <aok1425@gmail.com>
2014-07-02 16:26:00 -0700

    removed sens info

--- python/sensitive_info.py
+++
@@ -1,8 +0,0 @@
-key='AIzaSyBsbGsLbD2hM5jr1bewKc6hotr3iV1lpmw'
-bingkey='Aigw5zUPIFl1h-DVWxs3co1hFyupx-K1oWe8ss2SRpdTfQJKGzILySBUdQ0GBFH3'
-
-#for yelp
-consumer_key = 'p0z-o-8cwOH7c5h4GO8vhg'
-consumer_secret = 'qwPaxGEydLqHlNTYTAls-AGwy28'
-token = 'VPHPmXFuhRDtEYWPL56pAPgPFOdc1Gk0'
-token_secret = 'SChhpSGhdwKgGGoqjQ-vplr-0C4'
